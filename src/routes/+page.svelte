0b8ed7587bb224e914146ed8bf2377d9f0b5cff8
weeb .js <ilyoreo99@gmail.com>
2024-05-06 06:00:08 +0000

    progress ✨

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,48 @@
-<button class="btn">Hello daisyUI</button>
+<head>
+    <title>RattleRoar</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+</head>
+
+<body>
+  
+    <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            </div>
+            </div>
+            <p class="font-bold text-2xl">RattleRoar</p>  
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
+       </div>
+        <div class="navbar-end">
+          <a class="btn btn-neutral bg-orange bg-orange" href="/login">Login
+            <ion-icon name="log-in-outline"></ion-icon>
+          </a>
+          </div>
+      </div> 
+      <div class="flex justify-left">
+        <div class="flex flex-col items-left text-left gap-6 max-w-lg" style="margin-top: 20vh; margin-left: 5vh;">
+     
+            <h1 class="font-bold justify-left text-5xl fade-up">
+                Welcome to
+                <span class="text-orange-600">RattleRoar</span>
+            </h1>
+    
+            <span class="fade-up" style="animation-delay: 1s;">
+              Welcome to RattleRoar, where making friends is effortless! Connect, chat, and build meaningful relationships. 
+            </span>
+    
+            <div class="flex gap-4">
+                <a class="btn btn-neutral bg-orange slide-in" href="/register" style="animation-delay: 2s;">
+                    Get started
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </a>
+            </div>
+        </div>
+    </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    
+</body>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,48 @@
+<head>
+    <title>Login | RattleRoar</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+</head>
+
+<script>
+    import { Turnstile } from 'svelte-turnstile';
+    //TODO: Work on signin querry
+    /**
+     * @param {CustomEvent} token
+     */
+    const setToken = (token) => {
+        console.log(token);
+    }
+</script>
+
+<body>
+    <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content">
+            <h1 class="text-4xl font-bold">
+                Login
+            </h1>
+            <p class="text-lg text-gray-500 mt-2 create-account" style="margin-top: -20px;">
+                Don't have an account? <a class="link underline" href="/register">Register</a>
+            </p>
+            <div class="input-group">
+                <label class="input input-bordered flex items-center">
+                    <img src="images/email.svg" alt="fontawesome-icon" class="w-4 h-4 opacity-70" />
+                    <input type="text" class="grow" placeholder="Email" />
+                </label>
+                <label class="input input-bordered flex items-center second-label">
+                    <img src="images/password.svg" alt="fontawesome-icon" class="w-4 h-4 opacity-70" />
+                    <input type="password" class="grow" placeholder="Password" />
+                </label>
+                <p class="text-sm text-gray-500 mt-1" style="margin-top: -10px;">
+                    <a class="underline" href="/reset">Forgot your password?</a>
+                </p>
+                <Turnstile siteKey="0x4AAAAAAAVhwQ_4KwtEYmQ4" on:turnstile-callback={setToken} />
+                <button class="btn btn-neutral text-2x bg-orange" style="color: white;">
+                    Log In <ion-icon name="log-in-outline"></ion-icon>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
