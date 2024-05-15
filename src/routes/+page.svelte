2c544abc41759080b795a30781bc660209792d0d
weeb js <tahfaric@nycstudents.net>
2024-05-15 04:01:09 +0000

    progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,33 +11,29 @@
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
             </div>
             </div>
-            <p class="font-bold text-2xl">RattleRoar</p>  
-        </div>
-        <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal px-1">
-       </div>
-        <div class="navbar-end">
-          <a class="btn btn-neutral bg-orange bg-orange" href="/login">Login
-            <ion-icon name="log-in-outline"></ion-icon>
-          </a>
-          </div>
+            <div class="avatar">
+              <img src="src/images/logo.png" alt="avatar" />
+</div>
+	</div>
       </div> 
       <div class="flex justify-left">
         <div class="flex flex-col items-left text-left gap-6 max-w-lg" style="margin-top: 20vh; margin-left: 5vh;">
      
             <h1 class="font-bold justify-left text-5xl fade-up">
-                Welcome to
-                <span class="text-orange-600">RattleRoar</span>
+                No slogans, just
+                <span class="text-warning">Chicken</span>
             </h1>
     
             <span class="fade-up" style="animation-delay: 1s;">
               Welcome to RattleRoar, where making friends is effortless! Connect, chat, and build meaningful relationships. 
             </span>
     
-            <div class="flex gap-4">
-                <a class="btn btn-neutral bg-orange slide-in" href="/register" style="animation-delay: 2s;">
-                    Get started
-                    <ion-icon name="arrow-forward-outline"></ion-icon>
+            <div class="flex gap-2.5">
+                <a class="btn btn-solid-warning" href="/register" style="animation-delay: 2s;">
+                    Invite
+                </a>
+                <a class="btn btn-solid-warning" href={" "}>
+                  Support Server
                 </a>
             </div>
         </div>
